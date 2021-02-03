651389adb2f2bfbb5ca08aff2e9eca9e23b955a1
Yanis <yanis.bekhtaoui@gmail.com>
2021-02-03 12:26:37 -0800

    [CARD] almost ready

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -11,38 +11,64 @@
       </div>
       <div>
           <div class="table-head">
-              <p>Ressource</p>
-              <p>Current</p>
-              <p>Actions</p>
+                    <p>Ressource</p>
+                    <p>Current</p>
+                    <p>Actions</p>
           </div>
-          <div>
+          <div class="table-body">
+              <div class="profile-info">
+                  <div>
+                      <img class="picture" src="../assets/male.svg" alt="picture">
+                      <div class="infos">
+                          <h4>Project Manager</h4>
+                          <span>john Smith</span>
+                          <span>35 Hours</span>
+                      </div>
+                  </div>
+              </div>
+              <p>Hello</p>
+              <p>Hello</p>
+              <p>Hello</p>
+          </diV>
+          <div id="elevate" class="table-body">
+              <div class="profile-info">
+                  <div>
+                      <img class="picture" src="../assets/femme.svg" alt="picture">
+                      <div class="infos">
+                          <h4>Senior Developer</h4>
+                          <span>Mike Brown</span>
+                          <span>50H</span>
+                      </div>
+                  </div>
+              </div>
+              <p>Hello</p>
+              <p>Hello</p>
+              <p>Hello</p>
+          </diV>
+          <div class="table-body">
+              <div class="profile-info">
+                  <div>
+                      <img class="picture" src="../assets/femme.svg" alt="picture">
+                      <div class="infos">
+                          <h4>Web Designer</h4>
+                          <span>Jane Doe</span>
+                          <span>25H</span>
+                      </div>
+                  </div>
+              </div>
               <p>Hello</p>
               <p>Hello</p>
               <p>Hello</p>
           </diV>
       </div>
-      <!-- <table class="table">
-          <thead>
-              <tr>
-                  <th>Ressource</th>
-                  <th>Current task</th>
-                  <th>Actions</th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr>
-                  <td>Hello</td>
-                  <td>Hello</td>
-                  <td>Hello</td>
-              </tr>
-          </tbody>
-      </table> -->
   </div>
 </template>
 
 <script>
 export default {
-    name:"Card"
+    name:"Card",
+
+
 }
 </script>
 
@@ -56,26 +82,50 @@ export default {
     margin-left: 30px;
     margin-right: 30px;
 }
-/* table{
-    width: 100%;
-    border-spacing: 0px;
-}
-table thead tr{
-    height: 40px;
-    background-color: #eaf1f8;
-} */
-.table-head{
+.table-head {
     display: flex;
     height: 40px;
     background-color: #eaf1f8;
     border-radius: 10px;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
 }
 .table-head p {
     margin-bottom: 0px;
 }
-/* table tr:last-child td:first-child {
-    border-bottom-left-radius: 10px;
-} */
+
+.table-body{
+    display: flex;
+    justify-content: space-between;
+    margin-top:10px;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-top: 10px;
+}
+.picture{
+    width: 45px;
+    margin-right: 15px;
+}
+
+.profile-info div:first-child{
+    display: flex;
+}
+.infos{
+    display: flex;
+    flex-direction: column;
+}
+.table-head :first-child{
+    padding-left: 15px;
+}
+.table-head :last-child{
+    padding-right: 15px;
+}
+#elevate{
+   box-shadow:rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;;
+   border-radius: 20px;
+}
+
+
+
+
 </style>
